9859c983281d0d63d12be91ee47539f3bf8d18bd
82102 <82102@Gram-san>
2022-06-17 16:54:38 +0900

    test

--- src/main/java/project04/main.java
+++ src/main/java/project04/main.java
@@ -6,7 +6,7 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// test
+		// test dgdfgsdgsgsdgdsf
 		DAO dao = new DAO();
 		Account a1 = dao.getAccount(1);
 		System.out.println(a1.getBirthday());
