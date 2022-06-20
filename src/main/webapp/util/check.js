fb9e0e19648aec8d046fe20dd96d383d5d12ed6d
skawn <skawn@DESKTOP-21361.21361>
2022-06-20 18:38:35 +0900

    js 공통모듈 및 소스작성 규칙 초안

---
+++ src/main/webapp/util/check.js
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+// 널값 및 데이터 없음 체크.
+function isEmpty(arg){
+	if(arg == "" || arg == null || arg == undefined || 
+		(arg != null && typeof arg == "object" && !Object.keys(arg).length)){
+		return true;
+	}
+	return false;
+}
+
+// 문자열에 공백이 있는지 체크.
+function blankCheck(arg){
+	var pattern = /\s/g; // 스페이스, 탭 체크.
+	if(arg.value.match(pattern)){
+		return true;
+	}
+	return false;
+}
